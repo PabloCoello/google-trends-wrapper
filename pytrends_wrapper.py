8d3b216f33f16f7a4f5f111c2160ea466c3471c3
PabloC <pablo.coello@outlook.es>
2020-04-04 13:07:06 +0200

    removed country filter

--- pytrends_wrapper.py
+++ pytrends_wrapper.py
@@ -1,33 +1,7 @@
 class googleTrends():
     def __init__(self, conf):
         self.conf = conf
-
-        self.countries_dict = {
-            'Guatemala':'GT', 
-            'Colombia':'CO', 
-            'Spain':'ES', 
-            'Mexico':'MX', 
-            'Canada':'CA', 
-            'India':'IN',
-            'Venezuela, RB':'VE', 
-            'Argentina':'AR', 
-            'United States':'US', 
-            'Chile':'CL', 
-            'Peru':'PE',
-            'Ecuador':'EC', 
-            'Cameroon':'CM', 
-            'United Kingdom':'GB', 
-            'Ireland':'IE', 
-            'Australia':'AT',
-            'RSA':'ZA', 
-            'Philippines':'PH', 
-            'Czechia':'CZ',
-            'Luxembourg':'LU'
-        }
-        '''
-        #self.countries_dict = {key: countries[key] for key in self.conf['countries']}
-        '''
-        self.world_panel = self.get_world_panel()
+        self.panel = self.get_world_panel()
         
     def get_world_panel(self):
         '''
@@ -43,15 +17,14 @@ class googleTrends():
         wdata = {}
         searches = {}
         for geo in world_data.iterrows():
-            if geo[1]['geoCode'] in self.countries_dict.values():
-                searches[geo[1]['geoCode']] = self.get_searches(
-                    key_word=self.conf['ggt_key_word'],
-                    state=geo[1]['geoCode'],
-                    region=None,
-                    start_date=self.conf['start_date'],
-                    end_date=self.conf['end_date']
-                )
-                wdata[geo[1]['geoCode']] = geo[1][self.conf['ggt_key_word']]
+            searches[geo[1]['geoCode']] = self.get_searches(
+                key_word=self.conf['ggt_key_word'],
+                state=geo[1]['geoCode'],
+                region=None,
+                start_date=self.conf['start_date'],
+                end_date=self.conf['end_date']
+            )
+            wdata[geo[1]['geoCode']] = geo[1][self.conf['ggt_key_word']]
         toret = self.bilateral_adjust(searches, wdata)
         toret = self.build_df(toret)
         return(toret)
@@ -86,7 +59,7 @@ class googleTrends():
 
     def get_searches(self, key_word, state, region, start_date, end_date):
         '''
-        Returns temporal result for a unic individual.
+        Returns temporal result for an unique individual.
         '''
         if region != None:
             geo = '{}-{}'.format(state,region)
@@ -125,3 +98,11 @@ class googleTrends():
             matrix['countryCode'] = df[0]
             toret = toret.append(matrix)
         return(toret)
+
+if __name__ == '__main__':
+    conf = {
+    'ggt_key_word':
+    'ggt_state':
+    'start_date':
+    'end_date':
+    }
